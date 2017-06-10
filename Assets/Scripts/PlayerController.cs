53aaa455f17ef847483f06ace8758db7a4c0f7fa
syuukouha <252747500@qq.com>
2017-06-10 21:17:58 +0900

    選択肢

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private Animator playerAnimator;
+    [SerializeField]
+    private Animator dragonAnimator;
+    [SerializeField]
+    private Transform cube;
+    // Use this for initialization
+    void Start()
+    {
+        playerAnimator.SetBool("Walk", true);
+        transform.DOMoveX(-2.0f, 5.0f).OnComplete(()=> {
+            playerAnimator.SetBool("Walk", false);
+            dragonAnimator.SetBool("Attack", true);
+            cube.transform.DOMoveZ(1.0f, 1.0f).SetDelay(3.0f);
+        });
+	}
+
+}
